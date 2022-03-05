cd2058579b4d597562d865ba42d564c0a24d5a00
Redcker <admin@yangyingqi.com>
2022-03-05 23:12:24 +0800

    优化脚本，补充停止脚本说明

--- jd/jx_cfd_hb.py
+++ jd/jx_cfd_hb.py
@@ -1,24 +1,22 @@
-import json
-import datetime
-import requests
-from apscheduler.schedulers.blocking import BlockingScheduler
 import logging
+import datetime
+import json
 
-logger = logging.getLogger()
-fh = logging.FileHandler('cfd.log', encoding='utf-8')
-formatter = logging.Formatter("%(asctime)s - %(message)s")
-fh.setFormatter(formatter)
-logger.addHandler(fh)
-logger.setLevel(logging.INFO)
-with open('cfd_config.json', 'r', encoding='utf-8') as f:
-    CONFIG = json.loads(f.read())
-token = CONFIG['pushplus_token']
-cookie = CONFIG['cookie']
-advance_time = CONFIG['advance_time']
-offset_time = 0.01
+try:
+    from apscheduler.schedulers.blocking import BlockingScheduler
+    from fake_useragent import UserAgent
+    from tzlocal import get_localzone_name
+    import requests
+    import pytz
+except Exception as e:
+    logging.error(f'依赖导入错误：{e}')
+    exit(1)
 
 
 def send_to_wechat(title):
+    """
+    微信推送
+    """
     data = {
         'title': f'财富岛红包状态：{title}',
         'content': '请按照状态提示进行对应操作',
@@ -29,7 +27,10 @@ def send_to_wechat(title):
 
 
 def get_next_time():
-    now_time = datetime.datetime.now()
+    """
+    获取下一个整点的时间戳
+    """
+    now_time = datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai'))
     str_time = (now_time + datetime.timedelta(hours=1)).strftime("%Y-%m-%d %H:00:00")
     timestamp = int(datetime.datetime.strptime(str_time, "%Y-%m-%d %H:%M:%S").timestamp())
     return timestamp
@@ -37,27 +38,56 @@ def get_next_time():
 
 class JxCFD(object):
     def __init__(self, cookie):
+        """
+        初始化财富岛类
+        """
         self.cookie = cookie
         self.session = requests.session()
         self.session.headers = {
-            'referer': 'https://st.jingxi.com/',
-            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
-            'cookie': self.cookie
+            "Host": "m.jingxi.com",
+            "Accept": "*/*",
+            "Connection": "keep-alive",
+            'referer': 'https://st.jingxi.com/fortune_island/index2.html?ptag=7155.9.47&sceneval=2&sid=6f488e2778fa2db09a39f105577da07w',
+            'user-agent': f'jdpingou;android;5.21.4;appBuild/20596;session/332;pap/JA2019_3111789;ef/1;{UserAgent().random}',
+            'cookie': self.cookie,
+            "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br"
         }
 
     def get_cfd_url(self):
+        """
+        获取最新100红包url
+        """
         url = 'https://m.jingxi.com/jxbfd/user/ExchangeState?strZone=jxbfd&dwType=2&sceneval=2&g_login_type=1'
         ret = self.session.get(url).json()
-        dwLvl = ret['hongbao'][0]['dwLvl']
-        pool = ret['hongbaopool']
-        new_url = f'https://m.jingxi.com/jxbfd/user/ExchangePrize?strZone=jxbfd&dwType=3&dwLvl={dwLvl}&ddwPaperMoney=100000&strPoolName={pool}&sceneval=2&g_login_type=1'
+        try:
+            dwLvl = ret['hongbao'][0]['dwLvl']
+            pool = ret['hongbaopool']
+        except KeyError:
+            logging.error('获取最新url失败，可能是cookie已过期')
+            send_to_wechat('cookie已失效')
+            return
+        else:
+            new_url = f'https://m.jingxi.com/jxbfd/user/ExchangePrize?strZone=jxbfd&dwType=3&dwLvl={dwLvl}&ddwPaperMoney=100000&strPoolName={pool}&sceneval=2&g_login_type=1'
         return new_url
 
     def exchange_red_package(self):
+        """
+        兑换红包
+        """
         cfd_url = self.get_cfd_url()
-        while datetime.datetime.now().timestamp() < (next_timestamp + advance_time):  # 未到时间，无限循环
+        if not cfd_url:
+            return
+        while datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai')).timestamp() < (
+                next_timestamp - advance_time):  # 未到时间，无限循环
             pass
-        ret = self.session.get(cfd_url).json()
+        try:
+            start_time = datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai')).timestamp()
+            ret = self.session.get(cfd_url).json()
+            spend_time = datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai')).timestamp() - start_time  # 请求花费时间
+        except requests.exceptions.JSONDecodeError:
+            logging.error('cookie认证失败，请不要担心，这个是偶发情况。此次请求停止，等待下次请求')
+            return
         if ret['iRet'] == 0:
             title = '抢到了'
         else:
@@ -65,11 +95,7 @@ class JxCFD(object):
         logging.info(f"抢购结果：{title}")
         ret_code = ret['iRet']
         if ret_code in [2013, 2016]:  # 抢早或者抢迟不推送至微信
-            if ret_code == 2013:  # 迟了
-                new_advance_time = advance_time + offset_time
-            else:  # 早了
-                new_advance_time = advance_time - offset_time
-            CONFIG['advance_time'] = new_advance_time
+            CONFIG['advance_time'] = spend_time
             with open('cfd_config.json', 'w', encoding='utf-8') as fp:  # 更新配置
                 json.dump(CONFIG, fp, indent=2)
         else:
@@ -77,9 +103,33 @@ class JxCFD(object):
 
 
 if __name__ == '__main__':
+    logger = logging.getLogger()  # 日志配置
+    fh = logging.FileHandler('cfd.log', encoding='utf-8')
+    formatter = logging.Formatter("%(asctime)s - %(message)s")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.setLevel(logging.INFO)
+    logging.getLogger('apscheduler').setLevel(logging.ERROR)  # 不输出apscheduler的非错误日志
+    with open('cfd_config.json', 'r', encoding='utf-8') as f:
+        try:
+            CONFIG = json.load(f)  # 读取配置
+        except json.decoder.JSONDecodeError:
+            logging.error('配置读取错误，请检查配置文件格式是否正确')
+            exit(1)
+    token = CONFIG['pushplus_token']  # 读取pushplus token
+    cookie = CONFIG['cookie']  # 读取token
+    advance_time = CONFIG['advance_time']  # 提前时间
+    offset_time = 0.01
+    if get_localzone_name() != 'Asia/Shanghai':
+        logging.error('系统时区不是中国时区，脚本将使用中国时区运行，为了更准确的进行抢购，请最好主动调整系统时区')
     next_timestamp = get_next_time()
     jx_cfd = JxCFD(cookie)
     scheduler = BlockingScheduler()
-    scheduler.add_job(func=jx_cfd.exchange_red_package, trigger='cron', minute=59, id='jx_cfd',
-                      timezone='Asia/Shanghai')
-    scheduler.start()
+    try:
+        scheduler.add_job(func=jx_cfd.exchange_red_package, trigger='cron', minute=59, id='jx_cfd',
+                          timezone='Asia/Shanghai')
+    except Exception as e:
+        logging.error(f'启动定时任务失败，具体错误为：{e}')
+    else:
+        logging.info('定时脚本启动成功')
+        scheduler.start()
